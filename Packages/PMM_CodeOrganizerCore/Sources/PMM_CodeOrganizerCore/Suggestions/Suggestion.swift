5d25343cf82363b0ce5542fc6208c1d9f7b15ba9
Pedro M Moreno <morenogonzalezpedro93@gmail.com>
2025-08-25 20:47:31 +0200

    (feature): suggestion type

--- Packages/PMM_CodeOrganizerCore/Sources/PMM_CodeOrganizerCore/Suggestions/Suggestion.swift
+++ Packages/PMM_CodeOrganizerCore/Sources/PMM_CodeOrganizerCore/Suggestions/Suggestion.swift
@@ -1,8 +1,40 @@
 //
-//  File.swift
+//  Suggestion.swift
 //  PMM_CodeOrganizerCore
 //
-//  Created by Pedro M Moreno on 25/8/25.
+//  Created by Pedro M Moreno.
 //
 
 import Foundation
+
+public enum SuggestionSeverity: String {
+    case info = "Info"
+    case warning = "Warning"
+}
+
+public enum SuggestionCategory: String {
+    case imports
+    case longFunc
+    case longType
+    case tooManyTopLevel
+    case extensionStoredProp
+    case manyParams
+    case highComplexity
+    case longFile
+}
+
+public struct Suggestion {
+    public let line: Int         
+    public let message: String
+    public let severity: SuggestionSeverity
+    public let category: SuggestionCategory
+    
+    public init(
+        line: Int,
+        message: String,
+        severity: SuggestionSeverity,
+        category: SuggestionCategory
+    ) {
+        self.line = line; self.message = message; self.severity = severity; self.category = category
+    }
+}
